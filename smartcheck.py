b2fb75be44b5964fd2a3b80e99c949e85104aaed
John Fish <john@johnafish.ca>
2020-02-26 22:46:39 -0500

    Add Norvig's Spellcheck
    We now have spellcheck and n-gram prediction... now to combine the two!

--- smartcheck.py
+++ smartcheck.py
@@ -2,17 +2,18 @@
 Smartcheck: smart spellcheck in pure Python.
 
  FEATURES:
-  - n-gram language model 
+  - Norvig's autocorrect
+  - 3-gram language model 
 
  TODO:
-  - Contextual spellcheck from emails (enron corpus)
-  - Neural language model
+  - Combine Norvig + 3-gram approaches
+  - Build better error model with errors from text
+  - Save + pickle the trained 3-gram, language, error models
 """
 
-from nltk.corpus import reuters
-from nltk import bigrams, trigrams
+from nltk import trigrams, word_tokenize
 from collections import Counter, defaultdict
-from random import random
+import re
 
 class Smartcheck:
     """A smart spell checker.
@@ -21,38 +22,81 @@ class Smartcheck:
     """
 
     def __init__(self, corpus):
-        """Initializes model."""
-        self.model = defaultdict(lambda: defaultdict(lambda: 0))
+        """Initializes language model with trigram probabilities."""
         self.corpus = corpus
-        
-        for sentence in self.corpus.sents():
-            for w1, w2, w3 in trigrams(sentence, pad_right=True, pad_left=True):
-                self.model[(w1, w2)][w3] += 1
+        self.trigrams = defaultdict(lambda: defaultdict(lambda: 0))
+        self.model = {} 
+        self.pop_model()
+        self.pop_trigrams()
 
-        for wp in self.model:
-            total_count = float(sum(self.model[wp].values()))
-            for w3 in self.model[wp]:
-                self.model[wp][w3] /= total_count
+    def sentences(self, text):
+        """All sentences in a given text."""
+        return re.findall(r'([A-Z][^\.!?]*[\.!?])', text)
+
+    def words(self, text):
+        """All words in a given text."""
+        return re.findall(r'\w+', text)
+
+    def pop_model(self):
+        """Populate model with probability of word."""
+        word_counts = Counter(self.words(self.corpus))
+        N = sum(word_counts.values())
+        for word in word_counts:
+            self.model[word] = word_counts[word] / N
+        
+    def pop_trigrams(self):
+        """Populate self.trigrams with probabilities of next words"""
+        for sentence in self.sentences(self.corpus):
+            for w1, w2, w3 in trigrams(word_tokenize(sentence), pad_right=True, pad_left=True):
+                self.trigrams[(w1, w2)][w3] += 1
+
+        # Convert trigrams to probabilities
+        for wp in self.trigrams:
+            total_count = float(sum(self.trigrams[wp].values()))
+            for w3 in self.trigrams[wp]:
+                self.trigrams[wp][w3] /= total_count
 
     def predict(self, sentence):
-        words = sentence.split()[-2:]
+        """Predict the next words given the sentence."""
+        prev_two_words = sentence.split()[-2:]
+        options = dict(self.trigrams[tuple(prev_two_words)])
+        return options
 
-        options = dict(self.model[(words[0], words[1])])
-        threshold = random()
-        accumulator = 0.
+    def word_probability(self, word):
+        """Probability of a given word."""
+        if word in self.model:
+            return self.model[word]
+        return 0
 
-        for option, probability in options.items():
-            accumulator += probability
-            if accumulator >= threshold:
-                return option
-        return ""
+    def correction(self, word):
+        """Return the most probable correction."""
+        return max(self.candidates(word), key=self.word_probability)
 
+    def candidates(self, word):
+        """Candidate list of possible correct words."""
+        return (self.known([word]) or \
+                self.known(self.edits1(word)) or \
+                self.known(self.edits2(word)) or \
+                set([word]))
+
+    def known(self, words):
+        return set(w for w in words if w in self.model)
+
+    def edits1(self, word):
+        """All edits that are one edit away from `word`."""
+        letters    = 'abcdefghijklmnopqrstuvwxyz'
+        splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+        deletes    = [L + R[1:]               for L, R in splits if R]
+        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+        replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+        inserts    = [L + c + R               for L, R in splits for c in letters]
+        return set(deletes + transposes + replaces + inserts)
+    
+    def edits2(self, word): 
+        "All edits that are two edits away from `word`."
+        return (e2 for e1 in self.edits1(word) for e2 in self.edits1(e1))
 
 if __name__ == "__main__":
-    sc = Smartcheck(reuters)
-    sentence = "John Fish is the"
-
-    for i in range(10):
-        sentence += " {}".format(sc.predict(sentence))
-
-    print(sentence)
+    sentence = "This is a test sentence. This is another. This is a. Okay? Okay! Fine then."
+    sc = Smartcheck(sentence)
+    print(sc.correction("sentnce"))
